e1ad55cf3de20ce68cbce87b5475154467e1dede
unknown <mdimranhossain.ist@gmail.com>
2023-12-06 21:18:03 +0600

    update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = async () => {
     <div className="border-b">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+          <Link className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="text-xl font-extrabold">DHAKA STORE</p>
           </Link>
           <MainNav data={categories} />
